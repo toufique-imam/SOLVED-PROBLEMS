d45db7e9e199726abee2705317859abfdb85b737
Toufique Imam <2013nuhash@gmail.com>
2018-06-06 13:35:57 +0600

    Create UVA 108.cpp

---
+++ UVA 108.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+//#include<unordered_map>
+//#include<unordered_set>
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+/*
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+priority_queue<int, greater<int>, pairing_heap_tag> pq[1009];
+#define fbo         find_by_order
+#define ook         order_of_key
+*/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector< vector<ll> >matrix;
+#define gcd(A,B)        __gcd(A,B)
+#define PQ              priority_queue
+#define ii              int,int
+#define pll             pair<ll,ll>
+#define PI              2*acos(0.0)
+#define all(v)          v.begin(),v.end()
+#define pii             pair<ii>
+#define pb              push_back
+#define sf(x)           scanf("%I64d",&x)
+#define sff(x,y)        scanf("%I64d %I64d",&x,&y)
+#define sl(x)           scanf("%lld",&x)
+#define sll(x,y)        scanf("%lld %lld",&x,&y)
+#define ff              first
+#define ss              second
+#define segtree         lt=2*par,rg=2*par+1,mid=(st+en)/2
+#define read            freopen("input.txt","r",stdin)
+#define write           freopen("output.txt","w",stdout)
+#define fast            ios_base::sync_with_stdio(0),cin.tie(0)
+#define lead_zero(x)    __builtin_clzll(x)
+#define trail_zero(x)   __builtin_ctz(x)
+#define total_1s(x)     __builtin_popcount(x)
+#define max_ull         18446744073709551615
+#define max_ll          9223372036854775807;
+#define set(N,cur)      N=(N|(1LL<<cur))
+#define reset(N,cur)    N=(N&(~(1LL<<cur)))
+#define check(N,cur)    ((N&(1LL<<cur))==0)
+#define all(v)          v.begin(),v.end()
+#define reunique(v)     v.resize(std::unique(all(v)) - v.begin())
+/* int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; */ // 4 Direction
+/* int dx[] = {1,-1,0,0,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1,1,-1}; */ // 8 Direction
+/* int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; */ // Knight Direction
+/* int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; */ // Hexagonal Direction
+int dp[109][109];
+int ara[109][109];
+int main()
+{
+    int n;
+    while(scanf("%d",&n)==1 and n)
+    {
+        int ans=-1e9;
+        memset(dp,0,sizeof dp);
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
+            {
+                scanf("%d",&ara[i][j]);
+                dp[i][j]=ara[i][j];
+                if(j>1)
+                    dp[i][j]+=dp[i][j-1];
+                if(i>1)
+                    dp[i][j]+=dp[i-1][j];
+                if(i>1 and j>1)
+                    dp[i][j]-=dp[i-1][j-1];
+            }
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
+                for(int k=i; k<=n; k++)
+                    for(int l=j; l<=n; l++)
+                    {
+                        int q=dp[k][l];
+                        if(i>1)
+                            q-=dp[i-1][l];
+                        if(j>1)
+                            q-=dp[k][j-1];
+                        if(i>1 and j>1)
+                            q+=dp[i-1][j-1];
+                        ans=max(ans,q);
+                    }
+        printf("%d\n",ans);
+    }
+}
