cc5248c6fe9c8ab1fc5c10b622979629535682bd
Toufique Imam <2013nuhash@gmail.com>
2018-06-28 07:14:18 +0600

    Create USACO subset.cpp

---
+++ USACO subset.cpp
@@ -0,0 +1,84 @@
+/*
+ID: sabertooth
+LANG: C++11
+TASK: subset
+*/
+#include<bits/stdc++.h>
+//#include<unordered_map>
+//#include<unordered_set>
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+/*
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+priority_queue<int, greater<int>, pairing_heap_tag> pq[1009];
+#define fbo         find_by_order
+#define ook         order_of_key
+*/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector< vector<ll> >matrix;
+#define gcd(A,B)        __gcd(A,B)
+#define PQ              priority_queue< ll,vector<ll>,greater<ll> >pq;
+#define ii              int,int
+#define pll             pair<ll,ll>
+#define PI              2*acos(0.0)
+#define all(v)          v.begin(),v.end()
+#define pii             pair<ii>
+#define pb              push_back
+#define sf(x)           scanf("%I64d",&x)
+#define sff(x,y)        scanf("%I64d %I64d",&x,&y)
+#define sl(x)           scanf("%lld",&x)
+#define sll(x,y)        scanf("%lld %lld",&x,&y)
+#define ff              first
+#define ss              second
+#define segtree         lt=2*par,rg=2*par+1,mid=(st+en)/2
+#define read            freopen("input.txt","r",stdin)
+#define write           freopen("output.txt","w",stdout)
+#define fast            ios_base::sync_with_stdio(0),cin.tie(0)
+#define lead_zero(x)    __builtin_clzll(x)
+#define trail_zero(x)   __builtin_ctz(x)
+#define total_1s(x)     __builtin_popcount(x)
+#define max_ull         18446744073709551615
+#define max_ll          9223372036854775807;
+#define set(N,cur)      N=(N|(1<<cur))
+#define reset(N,cur)    N=(N&(~(1<<cur)))
+#define check(N,cur)    ((N&(1<<cur))==0)
+#define all(v)          v.begin(),v.end()
+#define reunique(v)     v.resize(std::unique(all(v)) - v.begin())
+#define loop            int test;scanf("%d",&test);for(int _T=1;_T<=test;_T++)
+/*int dx[] = {1,-1,0,0}, dy[] = {0,0,1,-1};   */// 4 Direction
+/* int dx[] = {1,-1,0,0,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1,1,-1}; */ // 8 Direction
+/* int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; */ // Knight Direction
+/* int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; */ // Hexagonal Direction
+
+ll dp[50][2500];
+ll fsum;
+ll n;
+ll recur(int posl,int sum)
+{
+    if(posl>n)
+        return 0;
+    if(fsum-sum==sum)
+        return 1;
+    if(fsum-sum<sum)
+        return 0;
+    ll &ans=dp[posl][sum];
+    if(ans!=-1)return ans;
+    ans=0;
+    ans+=recur(posl+1,sum+posl);
+    ans+=recur(posl+1,sum);
+    return ans;
+}
+int main()
+{
+    ifstream fin("subset.in");
+    ofstream fout("subset.out");
+    memset(dp,-1,sizeof dp);
+    fin>>n;
+    fsum=n*(n+1)/2;
+    fout<<recur(0,0)/2<<endl;
+}
